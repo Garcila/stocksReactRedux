34f3c3388463467f2560dd38be19b707c51eb8a3
German Arcila <germanarcila@gmail.com>
2017-09-08 09:06:34 -0400

    styled header

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = (props) => {
     return(
-        <nav className="navbar navbar-light bg-dark">
-          <a className="navbar-brand" href="#">{props.title}</a>
-        </nav>
+        <div className="header">
+          <a className="title" href="#">{props.title}</a>
+        </div>
     );
 }
 
